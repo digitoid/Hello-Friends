c849cff0b08669ad73ccc37f1034e09c24c4070d
Amanda <Admin@Amandas-MBP.local>
2013-12-12 14:39:43 -0500

    Learning more thing

--- File.c
+++ File.c
@@ -7,11 +7,18 @@
 //
 
 #include <stdio.h>
+#include "anotherfile.h"
+
+int weregoingtobeacalculator(int thing1, int thing2);
 
 main ()
 {
-    printf ("Hello, friends. I am here.\n");
-    int variable1 = 2;
-    int variable2 = 4;
-    printf ("%ispooky%ime\n", variable1 , variable2);
+    printf ("Hello, friends. I am here.\n %ispooky%ime ooooooh%i\n THE RAPTURE IS UPON US %i\n wwooooooooooooo %i\n", 2 , 4, weregoingtobeacalculator(6686,1), 666, weregoingtobeacalculator(1,666));
+    printf("\n\n\n%i\n", sum ((int)666,(int)1234));
+  
+}
+
+
+int weregoingtobeacalculator(int thing1, int thing2) {
+    return thing1*thing2*27;
 }
---
+++ anotherfile.c
@@ -0,0 +1,11 @@
+//
+//  anotherfile.c
+//  
+//
+//  Created by Digitoid on 11/21/13.
+//
+//
+
+#include <stdio.h>
+
+int sum (int bleh, int weh) {return bleh + weh*2; }
